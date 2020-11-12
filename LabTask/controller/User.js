b39821b102ed428a933dbd32d06e725d0f81a02f
Yeasir0995 <40072145+Yeasir0995@users.noreply.github.com>
2020-11-12 14:30:33 +0600

    Add files via upload

---
+++ LabTask/controller/User.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const userModel	= require.main.require('./models/userModel');
+const router = express.Router();
+
+router.get('/create', (req, res)=>{
+	res.render('user/create'); 
+})
+
+router.post('/create', (req, res)=>{
+
+	var user = {
+		username: 	req.body.username,
+		password: 	req.body.password,
+		type	: 	req.body.type
+	};
+
+	userModel.insert(user, function(status){
+		if(status){
+			res.redirect('/home/userlist');
+		}else{
+			res.redirect('user/create');
+		}
+	});
+})
+
+
+router.get('/edit/:id', (req, res)=>{
+
+	//var data = req.params.id;
+	//res.send(data);
+	var user ={
+		id: req.params.id,
+		username: 'alamin',
+		password: '123',
+		type: 'admin',
+	//	dept: 'EEE'
+	};
+
+	res.render('user/edit', user);
+})
+
+
+router.post('/edit/:id', (req, res)=>{
+
+	res.redirect('/home/userlist');
+	
+})
+
+router.get('/delete/:id', (req, res)=>{
+	var user ={
+		id: req.params.id,
+		username: 'alamin',
+		password: '123',
+		type:'admin'
+		//email: 'alamin@gmail.com',
+		//dept: 'CS'
+	};
+
+	res.render('user/delete', user);
+})
+
+router.post('/delete/:id', (req, res)=>{
+	
+	//delete from DB
+	res.redirect('/home/userlist');
+})
+
+module.exports = router;
+
